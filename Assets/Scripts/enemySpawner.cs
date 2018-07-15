0f63b0c8d9ee8861e61cc93bcc534bd81fc0c0e2
Thomas <thomas.simon@btopenworld.com>
2018-07-15 16:44:06 +0100

    Finished Spawner

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -4,46 +4,56 @@ using UnityEngine;
 
 public class enemySpawner : MonoBehaviour
 {
-    [SerializeField] protected List <GameObject> _enemyPrefabs;
+    [SerializeField] protected GameObject _enemyPrefab;
+    [SerializeField] protected int _maxEnemies;
+    protected List<GameObject> _listEnemies;
+
     [SerializeField] protected float _activateDelay;
-    [SerializeField] protected Transform _spawnLocation;
+    [SerializeField] protected Transform _spawnArea;
+    [SerializeField] protected float _spawnRadius;
+    protected Vector3 _spawnLocation;
 
     void Start ()
     {
+        _listEnemies = new List<GameObject>();
+
         initialSpawn();
 
         StartCoroutine(activateEnemy());
     }
 	
-	void Update ()
+	void createSpawnLocation()
     {
-
+        Vector3 _randomSpawn = new Vector3(Random.insideUnitSphere.x * _spawnRadius, transform.position.y, Random.insideUnitSphere.z * _spawnRadius);
+        _spawnLocation = _spawnArea.position + _randomSpawn;
     }
 
     void initialSpawn()
     {
-        for (int i = 0; i < _enemyPrefabs.Count; i++)
+
+        for (int i = 0; i < _maxEnemies; i++)
         {
-            Instantiate(_enemyPrefabs[i], _spawnLocation.position, Quaternion.identity);
-            _enemyPrefabs[i].SetActive(false);
+            createSpawnLocation();
+            GameObject _enmey = (GameObject)Instantiate(_enemyPrefab, _spawnLocation, Quaternion.identity);
+            _listEnemies.Add(_enmey);
+            _enmey.SetActive(false);
 
         }
     }
 
     IEnumerator activateEnemy()
     {
-        Debug.Log(_enemyPrefabs.Count);
         while(true)
         {
-
-            for (int i = 0; i < _enemyPrefabs.Count; i++)
+            for (int i = 0; i < _maxEnemies; i++)
             {
-                if (_enemyPrefabs[i].activeSelf == false)
+                if (_listEnemies[i].activeSelf == false)
                 {
-                    _enemyPrefabs[i].SetActive(true);
-                    Debug.Log("Did it");
+                    createSpawnLocation();
+                    _listEnemies[i].transform.position = _spawnLocation;
+                    _listEnemies[i].SetActive(true);
+                    break;
                 }
-
             }
 
             yield return new WaitForSeconds(_activateDelay);
